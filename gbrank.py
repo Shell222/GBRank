ed2bfdd6f50aa2bf425c4c2a259c30526f0ed3c2
Qiyuan Xu <qiyuan.xu6@gmail.com>
2018-07-31 18:25:50 +0800

    solve encoding issue

--- gbrank.py
+++ gbrank.py
@@ -1,15 +1,16 @@
 
 import regression_tree
 import copy
+import numpy as np
 
 class GBRank(object):
     
-    max_depth = 4
+    max_depth = 3
     feature_ratio = 0.7
     min_leaves = 50 
     size_param = 0
 
-    def __init__(self, data, iterations, margin, data_size, learning_ratio):
+    def __init__(self, data, iterations, margin, learning_ratio):
         """
         data is first col: query, final col: label
         """
@@ -29,8 +30,8 @@ class GBRank(object):
             dataset[line[0]].append(line[1:])
 
         for _, val in dataset.items():
-            val.sort(key=lambda i:i[-2], reversed=True)
-
+            val.sort(key=lambda i: -i[-2])
+        self.model_list = []
         self.model_list = self.train(dataset, data)
 
     def predict(self, dataset):
@@ -39,8 +40,9 @@ class GBRank(object):
             return prediction
         for i in dataset:
             for model in self.model_list:
-                previous_prediction[i[-1]] += model.predict(i[0: -2])
-            previous_prediction[i[-1]] /= len(self.model_list)
+                prediction[i[-1]] += model.predict(i[0: -2])
+            prediction[i[-1]] /= len(self.model_list)
+        # print(prediction)
         return prediction
 
     def train(self, dataset, data):
@@ -54,6 +56,7 @@ class GBRank(object):
                     next_row = copy.deepcopy(data[key])
                     next_row[-2] += self.learning_ratio * val[0] / val[1]
                     next_dataset.append(next_row[1: -1]) # first col is query, final col is index
+            next_dataset = np.array(next_dataset)
             tree = regression_tree.fit(next_dataset, self.max_depth, self.feature_ratio, self.min_leaves, self.size_param)
             model_list.append(tree)
         return model_list
@@ -66,7 +69,7 @@ class GBRank(object):
         
         # gradient should store [index, gradientSum, times]
         gradient = dict()
-        for i in len(single_query_dataset):
+        for i in range(len(single_query_dataset)):
             idx_i = single_query_dataset[i][-1]            
             val_i = previous_prediction[idx_i]
             for j in range(i + 1, len(single_query_dataset)):
@@ -82,7 +85,7 @@ class GBRank(object):
                     gradient[idx_i][1] += 1
                     if idx_j not in gradient:
                         gradient[idx_j] = [0, 0]
-                    gradient[idx_j] -= val_j - val_i + self.margin
+                    gradient[idx_j][0] -= val_j - val_i + self.margin
                     gradient[idx_j][1] += 1
         return gradient
 
@@ -91,16 +94,16 @@ def read_file(filename):
     special function to read test file
     """
     data = list()
-    first_line = True
-    with open(filename) as f:
-        for line in f.readlines():
-            if first_line:
-                first_line = False
-                continue
+    with open(filename, mode='r', encoding='gbk', errors='ignore') as f:
+        for line in f.readlines()[1:1000]:
             row = line.split('\t')
-            line_data = [row[1]] + [float(i) for i in row[2: -1]] + [row[0]]
+            line_data = [row[1]] + [float(i) for i in row[2: -1]] + [float(row[0])]
             data.append(line_data)
     return data
 
 if __name__ == '__main__':
-    pass
+
+    # data = read_file('./testdata.tsv')
+    data = read_file('../../base_train_file')
+
+    gb = GBRank(data, 10, 0.3, 1)
